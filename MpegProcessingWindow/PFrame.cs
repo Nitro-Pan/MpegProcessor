6c6bdab4367c8fc71e7d9bc817771f87edaa8330
Nitro-Pan <tate.smith.4242@gmail.com>
2022-03-07 00:10:47 -0800

    using separate c vectors, seems wrong somehow

--- MpegProcessingWindow/PFrame.cs
+++ MpegProcessingWindow/PFrame.cs
@@ -37,40 +37,41 @@ namespace MpegProcessingWindow {
             byte[,][,] tCbMacros = GetCbMacroBlocks();
             byte[,][,] tCrMacros = GetCrMacroBlocks();
 
-            byte[,][,] pYMacros = PrevFrame.GetYMacroBlocks();
-            byte[,][,] pCbMacros = PrevFrame.GetCbMacroBlocks();
-            byte[,][,] pCrMacros = PrevFrame.GetCrMacroBlocks();
+            var matrices = PrevFrame.GetMatrix().GetSubsampledImage();
+            byte[,] pYMatrix = matrices.Item1;
+            byte[,] pCbMatrix = matrices.Item2;
+            byte[,] pCrMatrix = matrices.Item3;
 
-            IntVector2[,] yVectors = CreateVectors(pYMacros, tYMacros);
-            IntVector2[,] cbVectors = CreateVectors(pCbMacros, tCbMacros);
-            IntVector2[,] crVectors = CreateVectors(pCrMacros, tCrMacros);
+            IntVector2[,] yVectors = CreateVectors(pYMatrix, tYMacros, MACROBLOCK_SIZE);
+            IntVector2[,] cbVectors = CreateVectors(pCbMatrix, tCbMacros, MACROBLOCK_SIZE / 2);
+            IntVector2[,] crVectors = CreateVectors(pCrMatrix, tCrMacros, MACROBLOCK_SIZE / 2);
 
             return (yVectors, cbVectors, crVectors);
         }
 
-        private IntVector2[,] CreateVectors(byte[,][,] prev, byte[,][,] curr) {
-            IntVector2[,] vectors = new IntVector2[prev.GetLength(0), prev.GetLength(1)];
+        private IntVector2[,] CreateVectors(byte[,] prev, byte[,][,] curr, int macroBlockSize) {
+            IntVector2[,] vectors = new IntVector2[curr.GetLength(0), curr.GetLength(1)];
 
             for (int x = 0; x < vectors.GetLength(0); x++) {
                 for (int y = 0; y < vectors.GetLength(1); y++) {
-                    vectors[x, y] = FindClosestMatch(prev, curr, x, y, SEARCH_RADIUS);
+                    vectors[x, y] = FindClosestMatch(prev, curr, x, y, SEARCH_RADIUS, macroBlockSize);
                 }
             }
 
             return vectors;
         }
 
-        private IntVector2 FindClosestMatch(byte[,][,] prev, byte[,][,] curr, int x, int y, int r) {
+        private IntVector2 FindClosestMatch(byte[,] prev, byte[,][,] curr, int x, int y, int r, int macroBlockSize) {
             IntVector2 offset = new();
-            int bestMAD = MAD(prev[x, y], curr[x, y]);
+            int bestMAD = MAD(CreateSingleMacro(prev, x * MACROBLOCK_SIZE, y * MACROBLOCK_SIZE, macroBlockSize), curr[x, y]);
 
             if (bestMAD == 0) return offset;
 
             for (int u = -r; u < r; u++) {
                 for (int v = -r; v < r; v++) {
                     if (x + u < 0 || x + u >= prev.GetLength(0) || y + v < 0 || y + v >= prev.GetLength(1)) continue;
-                    if (MAD(prev[x + u, y + v], curr[x, y]) < bestMAD) {
-                        bestMAD = MAD(prev[x + u, y + v], curr[x, y]);
+                    if (MAD(CreateSingleMacro(prev, x * MACROBLOCK_SIZE + u, y * MACROBLOCK_SIZE + v, macroBlockSize), curr[x, y]) < bestMAD) {
+                        bestMAD = MAD(CreateSingleMacro(prev, x * MACROBLOCK_SIZE + u, y * MACROBLOCK_SIZE + v, macroBlockSize), curr[x, y]);
                         offset = new IntVector2() { X = u, Y = v };
                     }
                 }
@@ -91,35 +92,47 @@ namespace MpegProcessingWindow {
             return result;
         }
 
+        private byte[,] CreateSingleMacro(byte[,] fullMatrix, int x, int y, int macroblockSize) {
+            byte[,] macro = new byte[macroblockSize, macroblockSize];
+            for (int u = 0; u < macroblockSize; u++) {
+                for (int v = 0; v < macroblockSize; v++) {
+                    if (x + u >= fullMatrix.GetLength(0) || y + v >= fullMatrix.GetLength(1)) continue;
+                    macro[u, v] = fullMatrix[x + u, y + v];
+                }
+            }
+            return macro;
+        }
+
         private ImageMatrix CalculateNewFrame() {
             if (PrevFrame == null) throw new InvalidOperationException();
 
-            byte[,][,] yM = PrevFrame.GetYMacroBlocks();
-            byte[,][,] cbM = PrevFrame.GetCbMacroBlocks();
-            byte[,][,] crM = PrevFrame.GetCrMacroBlocks();
+            var matrices = PrevFrame.GetMatrix().GetSubsampledImage();
+            byte[,] yM = matrices.Item1;
+            byte[,] cbM = matrices.Item2;
+            byte[,] crM = matrices.Item3;
 
-            byte[,][,] newY = new byte[yM.GetLength(0), yM.GetLength(1)][,];
-            byte[,][,] newCb = new byte[cbM.GetLength(0), cbM.GetLength(1)][,];
-            byte[,][,] newCr = new byte[crM.GetLength(0), crM.GetLength(1)][,];
+            byte[,][,] newY = new byte[yVectors.GetLength(0), yVectors.GetLength(1)][,];
+            byte[,][,] newCb = new byte[cbVectors.GetLength(0), cbVectors.GetLength(1)][,];
+            byte[,][,] newCr = new byte[crVectors.GetLength(0), crVectors.GetLength(1)][,];
 
-            for (int x = 0; x < yM.GetLength(0); x++) {
-                for (int y = 0; y < yM.GetLength(1); y++) {
+            for (int x = 0; x < newY.GetLength(0); x++) {
+                for (int y = 0; y < newY.GetLength(1); y++) {
                     IntVector2 offset = yVectors[x, y];
-                    newY[x, y] = yM[x + offset.X, y + offset.Y];
+                    newY[x, y] = CreateSingleMacro(yM, x * MACROBLOCK_SIZE + offset.X, y * MACROBLOCK_SIZE + offset.Y, MACROBLOCK_SIZE);
                 }
             }
 
-            for (int x = 0; x < cbM.GetLength(0); x++) {
-                for (int y = 0; y < cbM.GetLength(1); y++) {
-                    IntVector2 offset = cbVectors[x, y];
-                    newCb[x, y] = cbM[x + offset.X, y + offset.Y];
+            for (int x = 0; x < newCb.GetLength(0); x++) {
+                for (int y = 0; y < newCb.GetLength(1); y++) {
+                    IntVector2 offset = yVectors[x, y];
+                    newCb[x, y] = CreateSingleMacro(cbM, x * MACROBLOCK_SIZE + offset.X, y * MACROBLOCK_SIZE + offset.Y, MACROBLOCK_SIZE);
                 }
             }
 
-            for (int x = 0; x < crM.GetLength(0); x++) {
-                for (int y = 0; y < crM.GetLength(1); y++) {
-                    IntVector2 offset = crVectors[x, y];
-                    newCr[x, y] = crM[x + offset.X, y + offset.Y];
+            for (int x = 0; x < newCr.GetLength(0); x++) {
+                for (int y = 0; y < newCr.GetLength(1); y++) {
+                    IntVector2 offset = yVectors[x, y];
+                    newCr[x, y] = CreateSingleMacro(crM, x * MACROBLOCK_SIZE + offset.X, y * MACROBLOCK_SIZE + offset.Y, MACROBLOCK_SIZE);
                 }
             }
 
@@ -154,15 +167,15 @@ namespace MpegProcessingWindow {
         }
 
         public Line[] GetLines() {
-            Line[] lines = new Line[yVectors.GetLength(0) * yVectors.GetLength(1)];
+            Line[] lines = new Line[yVectors.GetLength(0) * yVectors.GetLength(1) + cbVectors.GetLength(0) * cbVectors.GetLength(1) + crVectors.GetLength(0) * crVectors.GetLength(1)];
 
             for (int x = 0; x < yVectors.GetLength(0); x++) {
                 for (int y = 0; y < yVectors.GetLength(1); y++) {
                     Line l = new Line() {
                         X1 = x * MACROBLOCK_SIZE,
                         Y1 = y * MACROBLOCK_SIZE,
-                        X2 = x * MACROBLOCK_SIZE + yVectors[x, y].X * MACROBLOCK_SIZE + 1,
-                        Y2 = y * MACROBLOCK_SIZE + yVectors[x, y].Y * MACROBLOCK_SIZE + 1,
+                        X2 = x * MACROBLOCK_SIZE + yVectors[x, y].X + 1,
+                        Y2 = y * MACROBLOCK_SIZE + yVectors[x, y].Y + 1,
                         StrokeThickness = 1,
                         Stroke = Brushes.GreenYellow
                     };
@@ -170,11 +183,43 @@ namespace MpegProcessingWindow {
                 }
             }
 
+            int cbOffset = yVectors.GetLength(0) * yVectors.GetLength(1);
+
+            for (int x = 0; x < cbVectors.GetLength(0); x++) {
+                for (int y = 0; y < cbVectors.GetLength(1); y++) {
+                    Line l = new Line() {
+                        X1 = x * MACROBLOCK_SIZE,
+                        Y1 = y * MACROBLOCK_SIZE,
+                        X2 = x * MACROBLOCK_SIZE + cbVectors[x, y].X + 1,
+                        Y2 = y * MACROBLOCK_SIZE + cbVectors[x, y].Y + 1,
+                        StrokeThickness = 1,
+                        Stroke = Brushes.CornflowerBlue
+                    };
+                    lines[y * cbVectors.GetLength(0) + x + cbOffset] = l;
+                }
+            }
+
+            int crOffset = cbOffset + cbVectors.GetLength(0) * cbVectors.GetLength(1);
+
+            for (int x = 0; x < crVectors.GetLength(0); x++) {
+                for (int y = 0; y < crVectors.GetLength(1); y++) {
+                    Line l = new Line() {
+                        X1 = x * MACROBLOCK_SIZE,
+                        Y1 = y * MACROBLOCK_SIZE,
+                        X2 = x * MACROBLOCK_SIZE + crVectors[x, y].X + 1,
+                        Y2 = y * MACROBLOCK_SIZE + crVectors[x, y].Y + 1,
+                        StrokeThickness = 1,
+                        Stroke = Brushes.Red
+                    };
+                    lines[y * crVectors.GetLength(0) + x + crOffset] = l;
+                }
+            }
+
             return lines;
         }
 
         public override ImageMatrix GetMatrix() { 
-            return targetFrame;
+            return calculatedFrame;
         }
 
         public override BitmapSource GetBitmap() {
